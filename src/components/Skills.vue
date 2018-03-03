180bd02e6816d59f4d8903d74c2c3d75d878985a
Karthik M S <ms18karthik@gmail.com>
2018-03-04 00:07:31 +0530

    Added form validator to the app

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,7 +3,8 @@
     <div class="holder">
 
     <form @submit.prevent="addSkill">
-      <input type="text" placeholder="Enter a skill you have..." v-model="skill">
+      <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+      <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
     </form>
       <ul>
         <li v-for="(data,index) in skills" :key='index'>{{ data.skill }}</li>
@@ -27,8 +28,14 @@ export default {
   },
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill})
-      this.skill = '';
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        } else {
+          console.log('Not Valid');
+        }
+      })
     }
   }
 }
@@ -73,5 +80,13 @@ input {
   font-size: 1.3em;
   background-color: #323333;
   color: #687F7F;
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
 }
 </style>
